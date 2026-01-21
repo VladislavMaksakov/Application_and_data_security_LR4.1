0079f1294d38fbe3b7ab2ea6812306784b7c7799
Владислав Максаков <vladislavmaksakov@MacBook-Air-Vladislav.local>
2026-01-21 16:36:01 +0200

    Аштфд мукіЄ

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+
+Console.Write("Введіть ім'я: ");
+string name = Console.ReadLine() ?? string.Empty;
+Console.Write("Дата народження: ");
+string dob = Console.ReadLine() ?? string.Empty;
+Console.Write("Секретне слово: ");
+string secret = Console.ReadLine() ?? string.Empty;
+
+string CalculateHash(string input)
+{
+   using var sha256 = SHA256.Create();
+   byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(input));
+   return Convert.ToHexString(bytes);
+}
+
+string privateKey = CalculateHash($"{name}{dob}{secret}");
+string publicKey = CalculateHash(privateKey);
+
+await File.WriteAllTextAsync("private_key.txt", privateKey);
+await File.WriteAllTextAsync("public_key.txt", publicKey);
+
+Console.WriteLine($"\nУспішно!\nPrivate Key: {privateKey[..10]}...\nPublic Key:  {publicKey[..10]}...");
+Console.WriteLine("Ключі збережено у папці з програмою.");
